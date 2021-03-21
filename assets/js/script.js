ece60fa8d1ae0ca54c46d56e4940d40dcce0ea04
davidboerm <davidboerm@gmail.com>
2021-03-20 23:55:54 -0500

    updated weather icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     var apiKey = '7a3ab3fca5c10f62a04c7656195495c6'
     var baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
     var baseUrlF = 'https://api.openweathermap.org/data/2.5/forecast?';
+    var iconBaseUrl = 'http://openweathermap.org/img/wn/';
     // WHEN I search for a city
     searchForm.submit(function(event) {
         event.preventDefault();
@@ -36,22 +37,26 @@ $(document).ready(function() {
         .then(function(data) {
             console.log(data);
             var city = data.name;
-            var date = moment().format('MM/DD/YYYY');
+            var date = moment().format('(M/DD/YYYY)');
             var temp = data.main.temp;
             var humidity = data.main.humidity;
-            var weatherIcon = data.weather.icon;
+            var weather = data.weather;
+            var iconUrl = iconBaseUrl + weather[0].icon + "@2x.png";
+            console.log(iconUrl);
             var wind = data.wind.speed;
             console.log(temp, humidity, wind);
             var cityDiv = $('<div class="city-name">');
             var tempDiv = $('<div class="temp-name">');
             var humidityDiv = $('<div class="humidity-name">');
-            var weatherIconDiv = $('<div class="icon-name">');
+            var weatherIconImg = $('<img src="" class="icon-name" />');
             var windDiv = $('<div class="wind-name">');
             cityDiv.text(city + " " + date);
+            weatherIconImg.attr('src', iconUrl);
             tempDiv.text("Temperature: " + temp + "\u00B0F");
             humidityDiv.text("Humidity: " + humidity + "\u0025");
             windDiv.text("Windspeed: " + wind + "mph")
             currentWeatherContainer.append(cityDiv);
+            currentWeatherContainer.append(weatherIconImg);
             currentWeatherContainer.append(tempDiv);
             currentWeatherContainer.append(humidityDiv);
             currentWeatherContainer.append(windDiv);
@@ -64,9 +69,40 @@ $(document).ready(function() {
         fetch(forecastUrl).then(function(response) {
             return response.json();
         }).then(function(data) {
+            console.log(forecastUrl);
             console.log("Five Day Forecast", data);
+            
             for (var i = 0; i < data.list.length; i++) {
-                
+                var isThreeOClock = data.list[i].dt_txt.search('21:00:00');
+                var cityName = data.city.name;
+                if (isThreeOClock > -1) {
+                    var forecast = data.list[i];
+                    var temp = forecast.main.temp;
+                    var humidity = forecast.main.humidity;
+                    var weatherIcon = forecast.weather;
+                    var iconUrl = iconBaseUrl + weatherIcon[0].icon + ".png";
+                    var wind = forecast.wind.speed;
+                    var date = moment(forecast.dt_txt).format('ddd M/DD');
+                    console.log(forecast, temp, humidity, weatherIcon, wind, cityName);
+                    var rowDiv = $('<div class="col-2">');
+                    var dateDiv = $('<div class="date-name">');
+                    var tempDiv = $('<div class="temp-name">');
+                    var humidityDiv = $('<div class="humidity-name">');
+                    var weatherImg = $('<img class="icon-name" />');
+                    weatherImg.attr('src', iconUrl);
+                    var windDiv = $('<div class="wind-name">');
+                    dateDiv.text(date);
+                    tempDiv.text("Temp: " + temp + "\u00B0F");
+                    humidityDiv.text("Hum: " + humidity + "\u0025");
+                    windDiv.text("Wind: " + wind + "mph");
+                    rowDiv.append(dateDiv);
+                    rowDiv.append(weatherImg);
+                    rowDiv.append(tempDiv);
+                    rowDiv.append(humidityDiv);
+                    rowDiv.append(windDiv);
+                    forecastWeatherContainer.append(rowDiv);
+
+                }
             }
         });
 
